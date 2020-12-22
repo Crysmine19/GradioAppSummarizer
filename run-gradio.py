b0434c0ad25b3c4734b2f62b616cb2a8de4a4334
Crysmine19 <54822190+Crysmine19@users.noreply.github.com>
2020-12-21 21:11:32 -0500

    Add files via upload

---
+++ run-gradio.py
@@ -0,0 +1,18 @@
+import gradio as gr
+from summarizer import Summarizer
+import torch
+
+
+def summarize_text(text):
+    model = Summarizer()
+    return model(text)
+
+inputs = gr.inputs.Textbox(lines=10, placeholder="Paste Text Here...")
+output_text = gr.outputs.Textbox()
+
+iface = gr.Interface(
+  fn=summarize_text, 
+  inputs= inputs, 
+  outputs= output_text)
+
+iface.launch()
